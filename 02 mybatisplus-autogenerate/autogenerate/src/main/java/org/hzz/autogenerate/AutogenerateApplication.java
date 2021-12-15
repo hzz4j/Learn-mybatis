88e127609ffc00e440b4432c1d2cd1698c8c5ab1
静默 <cau1403090523@gmail.com>
2021-12-15 15:46:56 +0800

    mybatis plus 自动生成代码实验

---
+++ 02 mybatisplus-autogenerate/autogenerate/src/main/java/org/hzz/autogenerate/AutogenerateApplication.java
@@ -0,0 +1,13 @@
+package org.hzz.autogenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutogenerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutogenerateApplication.class, args);
+	}
+
+}
---
+++ 02 mybatisplus-autogenerate/autogenerate/src/test/java/org/hzz/autogenerate/AutogenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hzz.autogenerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutogenerateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
